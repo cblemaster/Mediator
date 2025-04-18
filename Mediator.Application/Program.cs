c2b9c8b7abcc4e3cd880d1b63e7b6abe3300711a
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-18 16:41:32 -0400

    add endpoints

--- Mediator.Application/Program.cs
+++ Mediator.Application/Program.cs
@@ -2,6 +2,7 @@
 using Mediator.Application.Absractions;
 using Mediator.Application.Features.CreateOrder;
 using Mediator.Application.Features.GetOrder;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -12,5 +13,21 @@ builder.Services
 WebApplication app = builder.Build();
 
 app.MapGet("/", () => "Hello from Mediator!");
+
+app.MapGet(pattern: "/order/{id:guid}",
+    handler: async Task<Results<NotFound, Ok<GetOrderResponse>>> (Guid id, IQueryHandler<GetOrderQuery,
+    GetOrderResponse> handler) =>
+    {
+        GetOrderQuery query = new(id);
+        return TypedResults.Ok(await handler.Handle(query));
+    });
+
+app.MapPost(pattern: "/order",
+    handler: async Task<Results<BadRequest, Created<CreateOrderResponse>>> (CreateOrderCommand command,
+    ICommandHandler<CreateOrderCommand, CreateOrderResponse> handler) =>
+    {
+        _ = await handler.Handle(command);
+        return TypedResults.Created<CreateOrderResponse>("", null);
+    });
 
 app.Run();
