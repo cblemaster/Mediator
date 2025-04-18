4fe5f863635cb5f7e2639b8e45c1d808d9f4df15
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-18 12:39:02 -0400

    inject handlers

--- Mediator.Application/Program.cs
+++ Mediator.Application/Program.cs
@@ -1,5 +1,14 @@
 
+using Mediator.Application.Absractions;
+using Mediator.Application.Features.CreateOrder;
+using Mediator.Application.Features.GetOrder;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddScoped<ICommandHandler<CreateOrderCommand, CreateOrderResponse>, CreateOrderHandler>()
+    .AddScoped<IQueryHandler<GetOrderQuery, GetOrderResponse>, GetOrderHandler>();
+
 WebApplication app = builder.Build();
 
 app.MapGet("/", () => "Hello from Mediator!");
