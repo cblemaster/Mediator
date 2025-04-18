a4f1a28a3b92d4920e06873f39db0fb8f61b46e7
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-18 11:35:14 -0400

    add order model

---
+++ Mediator.Application/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Mediator.Application.Models;
+
+public sealed class Order(Guid id, string name, string description, decimal price, DateTimeOffset createDate)
+{
+    public Guid Id { get; set; } = id;
+    public string Name { get; set; } = name;
+    public string Description { get; set; } = description;
+    public decimal Price { get; set; } = price;
+    public DateTimeOffset CreateDate { get; set; } = createDate;
+}
