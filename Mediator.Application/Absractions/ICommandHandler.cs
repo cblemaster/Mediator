6d53a50bae6845194b3fa1b46fe5db7968cddb18
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-18 18:31:34 -0400

    add and update comments

--- Mediator.Application/Absractions/ICommandHandler.cs
+++ Mediator.Application/Absractions/ICommandHandler.cs
@@ -5,5 +5,11 @@ namespace Mediator.Application.Absractions;
 // and returns responses of type TResponse
 public interface ICommandHandler<in TCommand, TResponse>
 {
+    // Handles the given command asynchronously and returns a response
+    // Parameters:
+    // - command: The command object to be handled
+    // - cancellationToken: A token to observe for cancellation requests (optional)
+    // Returns:
+    // - A Task representing the asynchronous operation, with a result of type TResponse
     Task<TResponse> HandleAsync(TCommand command, CancellationToken cancellationToken = default);
 }
--- Mediator.Application/Absractions/IQueryHandler.cs
+++ Mediator.Application/Absractions/IQueryHandler.cs
@@ -5,11 +5,11 @@ namespace Mediator.Application.Absractions;
 // and returns responses of type TResponse
 public interface IQueryHandler<in TQuery, TResponse>
 {
-    // Handles the given query asynchronously and returns a response.
+    // Handles the given query asynchronously and returns a response
     // Parameters:
-    // - query: The query object to be processed.
-    // - cancellationToken: A token to observe for cancellation requests (optional).
+    // - query: The query object to be handled
+    // - cancellationToken: A token to observe for cancellation requests (optional)
     // Returns:
-    // - A Task representing the asynchronous operation, with a result of type TResponse.
+    // - A Task representing the asynchronous operation, with a result of type TResponse
     Task<TResponse> HandleAsync(TQuery query, CancellationToken cancellationToken = default);
 }
