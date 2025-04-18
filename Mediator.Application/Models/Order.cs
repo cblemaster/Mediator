1238cf35579cf45e08991aa9a7dea8d5cf12734b
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-18 18:32:02 -0400

    remove setters

--- Mediator.Application/Models/Order.cs
+++ Mediator.Application/Models/Order.cs
@@ -3,9 +3,9 @@ namespace Mediator.Application.Models;
 
 public sealed class Order(Guid id, string name, string description, decimal price, DateTimeOffset createDate)
 {
-    public Guid Id { get; set; } = id;
-    public string Name { get; set; } = name;
-    public string Description { get; set; } = description;
-    public decimal Price { get; set; } = price;
-    public DateTimeOffset CreateDate { get; set; } = createDate;
+    public Guid Id { get; } = id;
+    public string Name { get; } = name;
+    public string Description { get; } = description;
+    public decimal Price { get; } = price;
+    public DateTimeOffset CreateDate { get; } = createDate;
 }
