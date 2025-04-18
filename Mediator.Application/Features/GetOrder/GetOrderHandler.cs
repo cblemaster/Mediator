6d6ecfc9d26e95a085f3514b0c3b974f54f644de
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-18 12:35:38 -0400

    change interface implementation

--- Mediator.Application/Features/GetOrder/GetOrderHandler.cs
+++ Mediator.Application/Features/GetOrder/GetOrderHandler.cs
@@ -3,8 +3,8 @@ using Mediator.Application.Absractions;
 
 namespace Mediator.Application.Features.GetOrder;
 
-public sealed class GetOrderHandler : ICommandHandler<GetOrderQuery, GetOrderResponse>
+public sealed class GetOrderHandler : IQueryHandler<GetOrderQuery, GetOrderResponse>
 {
-    public Task<GetOrderResponse> Handle(GetOrderQuery command, CancellationToken cancellationToken = default) =>
+    public Task<GetOrderResponse> Handle(GetOrderQuery query, CancellationToken cancellationToken = default) =>
         throw new NotImplementedException();
 }
